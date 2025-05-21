6250c2a5455eec69b74e14d4b7ad6a07c0553176
hge2ne <hge2ne@gmail.com>
2025-05-21 18:57:50 +0900

    라우터설정완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col justify-center items-center h-screen bg-slate-800 text-white">
+    <h1 className="text-4xl font-bold text-sky-400 mb-4">
+      Vite + React + Tailwind CSS 설정 완료!
+    </h1>
+    <p className="text-lg text-green-400">
+      Tailwind 스타일이 잘 적용됩니다.
+    </p>
+  </div>
   )
 }
 
